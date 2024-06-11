b80dd353b94498343aff8ecf7870c21db8774d35
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2024-06-11 13:37:15 +0200

    feat: Initialize Next.js project

---
+++ project-bragi/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import SmallButtonWithIcon from "./components/smallButtonWithIcon";
+
+export default function Home() {
+  return (
+  <>
+    <SmallButtonWithIcon />
+  </>
+  );
+}
