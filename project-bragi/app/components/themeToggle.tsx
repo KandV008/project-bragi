992c88c84d27635d9057a51ac3a8492aff8be152
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2024-06-12 14:00:41 +0200

    refactor: Update Theme Toggle Component

--- project-bragi/app/components/themeToggle.tsx
+++ project-bragi/app/components/themeToggle.tsx
@@ -1,26 +1,49 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 const ThemeToggle = () => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark');
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
     } else {
-      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove("dark");
     }
   }, [theme]);
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    setTheme(theme === "light" ? "dark" : "light");
   };
 
   return (
-    <button onClick={toggleTheme} className="p-2 bg-gray-200 dark:bg-gray-700 rounded">
-      {theme === 'light' ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
-    </button>
+    <>
+      <button
+        onClick={toggleTheme}
+        id="theme-toggle"
+        className="absolute p-3 dark:bg-secondary0 bg-primary0  
+                  border-2 border-transparent top-5 right-5 w-fit rounded-2xl
+                  hover:bg-gray-400 hover:border-primary2 hover:border-2
+                  hover:dark:bg-gray-700 hover:dark:border-secondary0"
+      >
+        {theme === "light" ? (
+          <FontAwesomeIcon
+            icon={faMoon}
+            className="w-7  text-primary2"
+            id="theme-toggle-light-icon"
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faSun}
+            className=" w-7 text-secondary2"
+            id="theme-toggle-dark-icon"
+          />
+        )}
+      </button>
+    </>
   );
 };
 
