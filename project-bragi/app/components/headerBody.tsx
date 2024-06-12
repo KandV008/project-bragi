f50d5dda711373b4bec659026cb76f055d9fd386
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2024-06-12 13:32:41 +0200

    feat: Add Header Body Component

---
+++ project-bragi/app/components/headerBody.tsx
@@ -0,0 +1,36 @@
+import {
+  faUser,
+  faHeart,
+  faCartShopping,
+} from "@fortawesome/free-solid-svg-icons";
+import SmallButtonWithIcon from "./smallButtonWithIcon";
+import SearchBar from "./searchBar";
+import Logo from "./logo";
+
+export default function HeaderBody() {
+  return (
+    <>
+      <div className="flex flex-row justify-center items-center space-x-2">
+        <Logo />
+        <SearchBar />
+        <div className="flex flex-row">
+          <SmallButtonWithIcon
+            icon={faUser}
+            text={"Cuenta"}
+            subtext={"Iniciar sesión / Registrarse"}
+          />
+          <SmallButtonWithIcon
+            icon={faHeart}
+            text={"Favoritos"}
+            subtext={"Ver su lista de favoritos"}
+          />
+          <SmallButtonWithIcon
+            icon={faCartShopping}
+            text={"Cesta"}
+            subtext={"Ver su lista de la compra"}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
--- project-bragi/app/page.tsx
+++ project-bragi/app/page.tsx
@@ -1,36 +1,8 @@
-import {
-  faUser,
-  faHeart,
-  faCartShopping,
-} from "@fortawesome/free-solid-svg-icons";
-import SmallButtonWithIcon from "./components/smallButtonWithIcon";
-import SearchBar from "./components/searchBar";
-import Logo from "./components/logo";
+import HeaderBody from "./components/headerBody";
+
 
 export default function Home() {
   return (
-    <>
-      <div className="flex flex-row justify-between">
-        <SmallButtonWithIcon
-          icon={faUser}
-          text={"Cuenta"}
-          subtext={"Iniciar sesión / Registrarse"}
-        />
-        <SmallButtonWithIcon
-          icon={faHeart}
-          text={"Favoritos"}
-          subtext={"Ver su lista de favoritos"}
-        />
-        <SmallButtonWithIcon
-          icon={faCartShopping}
-          text={"Cesta"}
-          subtext={"Ver su lista de la compra"}
-        />
-      </div>
-
-      <SearchBar />
-
-      <Logo />
-    </>
+    <HeaderBody />
   );
 }
