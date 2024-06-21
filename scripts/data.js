7fa3619d9b59ff7106040740da8cf6a0d5dc2c95
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2024-06-21 13:20:08 +0200

    feat: Seed database with example data

---
+++ scripts/data.js
@@ -0,0 +1,165 @@
+/*
+    GUIDE
+        Colors:
+            P1 - Beige Sand
+            P3 - Sandalwood
+            P4 - Brown
+            P5 - Champagne
+            P6 - Silver gray
+            P7 - Graphite
+            P8 - Velvety black
+            Q2 - Electric green
+            Q3 - Caribbean Pirate
+            T3 - Precious Rose
+            H0 - Beige
+*/
+
+const docs = [
+    {
+        name: "Audéo Lumity L-R",
+        category: "EARPHONE",
+        price: 1299.00,
+        description: "Sample text 1",
+        colors: [
+            {
+                color: "P1",
+                images: [
+                    "./products/audeo-lumity/L-R/P1/PH_Packshot_Audeo_L-R_xReceiver_050-0889-P1.tif",
+                    "./products/audeo-lumity/L-R/P1/PH_Packshot_Audeo_L-R_xReceiver_without_logo_050-0889-P1.tif",
+                ],
+            },
+            {
+                color: "P3",
+                images: [
+                    "./products/audeo-lumity/L-R/P3/PH_Packshot_Audeo_L-R_xReceiver_050-0889-P3.tif",
+                    "./products/audeo-lumity/L-R/P3/PH_Packshot_Audeo_L-R_xReceiver_without_logo_050-0889-P3.tif",
+                ],
+            },
+            {
+                color: "P4",
+                images: [
+                    "./products/audeo-lumity/L-R/P4/PH_Packshot_Audeo_L-R_xReceiver_050-0889-P4.tif",
+                    "./products/audeo-lumity/L-R/P4/PH_Packshot_Audeo_L-R_xReceiver_without_logo_050-0889-P4.tif",
+                ],
+            },
+            {
+                color: "P5",
+                images: [
+                    "./products/audeo-lumity/L-R/P5/PH_Packshot_Audeo_L-R_xReceiver_050-0889-P5.tif",
+                    "./products/audeo-lumity/L-R/P5/PH_Packshot_Audeo_L-R_xReceiver_without_logo_050-0889-P5.tif",
+                ],
+            },
+            {
+                color: "P6",
+                images: [
+                    "./products/audeo-lumity/L-R/P6/PH_Packshot_Audeo_L-R_xReceiver_050-0889-P6.tif",
+                    "./products/audeo-lumity/L-R/P6/PH_Packshot_Audeo_L-R_xReceiver_without_logo_050-0889-P6.tif",
+                ],
+            },
+            {
+                color: "P7",
+                images: [
+                    "./products/audeo-lumity/L-R/P7/PH_Packshot_Audeo_L-R_xReceiver_050-0889-P7.tif",
+                    "./products/audeo-lumity/L-R/P7/PH_Packshot_Audeo_L-R_xReceiver_without_logo_050-0889-P7.tif",
+                ],
+            },
+            {
+                color: "P8",
+                images: [
+                    "./products/audeo-lumity/L-R/P8/PH_Packshot_Audeo_L-R_xReceiver_050-0889-P8.tif",
+                    "./products/audeo-lumity/L-R/P8/PH_Packshot_Audeo_L-R_xReceiver_without_logo_050-0889-P8.tif",
+                ],
+            },
+            {
+                color: "H0",
+                images: [
+                    "./products/audeo-lumity/L-R/H0/PH_Packshot_Audeo_L-R_xReceiver_050-0889-H0.tif",
+                    "./products/audeo-lumity/L-R/H0/PH_Packshot_Audeo_L-R_xReceiver_without_logo_050-0889-H0.tif",
+                ],
+            },
+        ],
+        include: ["Sample text 2", "Sample text 3"],
+        adaptation_range: "MILD",
+        dust_water_resistance: 1,
+        brand: "PHONAK",
+        location: "CIC",
+        level_of_discretion: "DISCREET",
+        degree_of_loss: "MILD",
+        uses: ["CHAT", "IN_GROUP", "LEISURE", "TELEPHONE", "TV"],
+    },
+    {
+        name: "Audéo Lumity L-RT",
+        category: "EARPHONE",
+        price: 1599.00,
+        description: "Sample text 4",
+        colors: [
+            {
+                color: "P1",
+                images: [
+                    "./products/audeo-lumity/L-RT/P1/PH_Packshot_Audeo_L-RT_xReceiver_050-1000-P1.tif",
+                    "./products/audeo-lumity/L-Rt/P1/PH_Packshot_Audeo_L-RT_xReceiver_without_logo_050-1000-P1.tif",
+                ],
+            },
+            {
+                color: "P3",
+                images: [
+                    "./products/audeo-lumity/L-RT/P3/PH_Packshot_Audeo_L-RT_xReceiver_050-1000-P3.tif",
+                    "./products/audeo-lumity/L-Rt/P3/PH_Packshot_Audeo_L-RT_xReceiver_without_logo_050-1000-P3.tif",
+                ],
+            },
+            {
+                color: "P4",
+                images: [
+                    "./products/audeo-lumity/L-RT/P4/PH_Packshot_Audeo_L-RT_xReceiver_050-1000-P4.tif",
+                    "./products/audeo-lumity/L-Rt/P4/PH_Packshot_Audeo_L-RT_xReceiver_without_logo_050-1000-P4.tif",
+                ],
+            },
+            {
+                color: "P5",
+                images: [
+                    "./products/audeo-lumity/L-RT/P5/PH_Packshot_Audeo_L-RT_xReceiver_050-1000-P5.tif",
+                    "./products/audeo-lumity/L-Rt/P5/PH_Packshot_Audeo_L-RT_xReceiver_without_logo_050-1000-P5.tif",
+                ],
+            },
+            {
+                color: "P6",
+                images: [
+                    "./products/audeo-lumity/L-RT/P6/PH_Packshot_Audeo_L-RT_xReceiver_050-1000-P6.tif",
+                    "./products/audeo-lumity/L-Rt/P6/PH_Packshot_Audeo_L-RT_xReceiver_without_logo_050-1000-P6.tif",
+                ],
+            },
+            {
+                color: "P7",
+                images: [
+                    "./products/audeo-lumity/L-RT/P7/PH_Packshot_Audeo_L-RT_xReceiver_050-1000-P7.tif",
+                    "./products/audeo-lumity/L-Rt/P7/PH_Packshot_Audeo_L-RT_xReceiver_without_logo_050-1000-P7.tif",
+                ],
+            },
+            {
+                color: "P8",
+                images: [
+                    "./products/audeo-lumity/L-RT/P8/PH_Packshot_Audeo_L-RT_xReceiver_050-1000-P8.tif",
+                    "./products/audeo-lumity/L-Rt/P8/PH_Packshot_Audeo_L-RT_xReceiver_without_logo_050-1000-P8.tif",
+                ],
+            },
+            {
+                color: "H0",
+                images: [
+                    "./products/audeo-lumity/L-RT/H0/PH_Packshot_Audeo_L-RT_xReceiver_050-1000-H0.tif",
+                    "./products/audeo-lumity/L-Rt/H0/PH_Packshot_Audeo_L-RT_xReceiver_without_logo_050-1000-H0.tif",
+                ],
+            },
+        ],
+        include: ["Sample text 5", "Sample text 6"],
+        adaptation_range: "MILD",
+        dust_water_resistance: 1,
+        brand: "PHONAK",
+        location: "CIC",
+        level_of_discretion: "DISCREET",
+        degree_of_loss: "MILD",
+        uses: ["CHAT", "IN_GROUP", "LEISURE", "TELEPHONE", "TV"],
+    },
+  ];
+  
+module.exports = { docs };
+  
--- scripts/seed.js
+++ scripts/seed.js
@@ -1,6 +1,8 @@
-require('dotenv').config({ path: '.env.local' });
+// seed.js
+require("dotenv").config({ path: ".env.local" });
 
-const { MongoClient, ServerApiVersion } = require('mongodb'); // Use require instead of import
+const { MongoClient, ServerApiVersion } = require("mongodb"); // Use require instead of import
+const { docs } = require('./data'); // Require docs from data.js
 
 const USERNAME = process.env.USER;
 const PASSWORD = process.env.PASSWORD;
@@ -12,20 +14,29 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // database and collection code goes here
+    const db = client.db("Product-DDBB");
+    const coll = db.collection("products");
+
+    // insert code goes here
+    const result = await coll.insertMany(docs);
+
+    // display the results of your operation
+    console.log(result.insertedIds);
+
+    console.log("Database seeded. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-run().catch(console.dir);
 
+run().catch(console.dir);
