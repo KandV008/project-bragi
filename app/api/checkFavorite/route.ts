36ff5aabac128a8d0da76cfb3329b3f998abebfa
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2024-07-08 19:13:23 +0200

    feat: Add PostgresSQL integration

---
+++ app/api/checkFavorite/route.ts
@@ -0,0 +1,18 @@
+import { checkFavorite } from "@/app/lib/action";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const productId = searchParams.get('productId');
+    const userId = searchParams.get('userId');
+  
+    try {
+      const isFav = await checkFavorite(userId, productId);
+      console.log(isFav)
+  
+      return NextResponse.json(isFav);
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    }
+  }
--- app/components/pages/search/product.tsx
+++ app/components/pages/search/product.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Image from "next/image";
 import { faCartShopping, faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { toggleFavorites } from "@/app/lib/action";
+import { useUser } from "@clerk/clerk-react";
+import { useState, useEffect } from "react";
 
 interface ProductProps {
   id: string;
@@ -11,10 +16,38 @@ interface ProductProps {
   price: string;
 }
 
-export default function Product({id, image, name, brand, price}: ProductProps) {
+export default function Product({
+  id,
+  image,
+  name,
+  brand,
+  price,
+}: ProductProps) {  
+  const { user } = useUser();
+  const [isFavourite, setIsFavourite] = useState(false);
+
+  useEffect(() => {
+    if (id && user) {
+      fetch(`/api/checkFavorite?productId=${id}&userId=${user.id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setIsFavourite(data)
+        })
+        .catch((error) => console.error("Error fetching product:", error));
+    }
+  }, [id, user])
+
+  const toggleFavourite = () => {
+    setIsFavourite(!isFavourite);
+  };
+
+  const textColor = isFavourite ? "text-red-500" : "text-primary2";
+  const bgColor = isFavourite ? "bg-red-200" : "bg-primary0";
+  const borderColor = isFavourite ? "border-red-500" : "border-primary2";
+
+
   return (
-    <Link
-      href={`/search/about/${id}`} 
+    <article
       className="flex flex-col justify-between items-center gap-3 p-3 md:p-4 xl:p-5
     bg-primary0 dark:bg-secondary2 border-solid border-2  border-primary2 dark:border-secondary1 text-primary2 dark:text-secondary0
     rounded rounded-tr-3xl 
@@ -41,36 +74,39 @@ export default function Product({id, image, name, brand, price}: ProductProps) {
         </div>
       </section>
       <section className="flex flex-row flex-wrap justify-center gap-3 md:gap-2 xl:gap-1">
-        <form method="post" action="">
-          <button
-            type="submit"
-            className="flex flex-row items-center justify-center md:justify-between md:px-2
-            rounded-2xl border-2 border-primary2  dark:border-secondary0  hover:bg-gray-400 hover:dark:bg-gray-700
-            h-8 w-12 md:w-24 md:h-10 xl:h-12 xl:w-40 "
-          >
-            <div className=" mr-0 md:mr-2 xl:mr-0">
-              <FontAwesomeIcon icon={faCartShopping} className="" />
-            </div>
-            <span className="hidden xl:block text-sm font-black">
-              Añadir a la cesta
-            </span>
-            <span className="hidden md:block xl:hidden text-sm font-black ">
-              Añadir
-            </span>
-          </button>
-        </form>
-        <form method="post" action="">
-          <button
-            type="submit"
-            className="rounded-2xl border-2 border-primary2 dark:border-secondary0 hover:bg-gray-400 hover:dark:bg-gray-700
-             h-8 w-12 md:size-10 xl:size-12"
-          >
-            <div className="">
-              <FontAwesomeIcon icon={faHeart} className="" />
-            </div>
-          </button>
-        </form>
+        <Link
+          href={`/search/about/${id}`}
+          className="flex flex-row items-center justify-center md:justify-between md:px-2
+          rounded-2xl border-2 border-primary2  dark:border-secondary0  hover:bg-gray-400 hover:dark:bg-gray-700
+          h-8 w-12 md:w-24 md:h-10 xl:h-12 xl:w-40 "
+        >
+          <div className=" mr-0 md:mr-2 xl:mr-0">
+            <FontAwesomeIcon icon={faCartShopping} className="" />
+          </div>
+          <span className="hidden xl:block text-sm font-black">
+            Ver producto
+          </span>
+          <span className="hidden md:block xl:hidden text-sm font-black ">
+            Ver más
+          </span>
+        </Link>
+        {!user ? (
+          <></>
+        ) : (
+          <form action={toggleFavorites}>
+            <input type="hidden" name="id" value={id} />
+            <button
+              type="submit"
+              onClick={toggleFavourite}
+              className={`rounded-2xl border-2 hover:bg-gray-400 hover:dark:bg-gray-700 h-8 w-12 md:size-10 xl:size-12 ${bgColor} ${borderColor} ${textColor}`}
+            >
+              <div className="">
+                <FontAwesomeIcon icon={faHeart} className="" />
+              </div>
+            </button>
+          </form>
+        )}
       </section>
-    </Link>
+    </article>
   );
 }
