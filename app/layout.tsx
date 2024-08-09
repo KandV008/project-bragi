1013af9912ee6e3e9c73f6dacf17d12d0852acc5
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2024-08-09 13:39:25 +0200

    refactor: Update Clerk Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,7 +74,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider localization={localization} publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider localization={localization} >
       <html lang="es">
         <head>
           <title>Project Bragi</title>
