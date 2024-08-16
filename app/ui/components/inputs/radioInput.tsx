719475d3e2c82b81fdbca77dd3f106f5e58e3d7e
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2024-08-16 17:26:42 +0200

    feat: Create Radio Input Component

---
+++ app/ui/components/inputs/radioInput.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
+
+interface RadioInputListProps {
+  name: string;
+  label: string;
+  list: any[];
+  valueOf: (type: string) => string;
+  type: string;
+  onChange?: (type: string) => (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function RadioInput({
+  name,
+  label,
+  list,
+  valueOf,
+}: RadioInputListProps) {
+  const [selectedValue, setSelectedValue] = useState<string | null>(null);
+    useState<ChangeEvent<HTMLInputElement> | null>(null);
+
+  const handleRadioChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    setSelectedValue(newValue);
+  };
+
+  const getRadioButtonStatus = (buttonName: string) => {
+    const baseClasses = "flex flex-row gap-2 items-center justify-start cursor-pointer py-1 px-2 rounded-2xl border-2";
+    return buttonName === selectedValue
+      ? `${baseClasses} text-rose-600 bg-rose-200 border-rose-600`
+      : `${baseClasses} bg-emerald-100 dark:bg-emerald-800  
+          text-emerald-900 dark:text-emerald-100
+          border-emerald-900 dark:border-emerald-100`;
+  };
+
+  return (
+    <section className="flex flex-col w-72">
+      <label
+        htmlFor={name}
+        className="bg-transparent 
+            w-3/4 md:w-9/12 font-extrabold text-lg cursor-pointer "
+      >
+        {label}
+      </label>
+      <div className="flex flex-col gap-2">
+      {list.map((element, index) => (
+        <article
+          key={element + "-" + index}
+          className={getRadioButtonStatus(valueOf(element))}
+        >
+          <input
+            type="radio"
+            id={name + "-" + index}
+            name={name}
+            value={valueOf(element)}
+            checked={selectedValue === valueOf(element)}
+            onChange={handleRadioChange}
+            required
+          />
+          <label htmlFor={name + "-" + index}> {element}</label>
+          <br />
+        </article>
+      ))}
+
+      </div>
+    </section>
+  );
+}
