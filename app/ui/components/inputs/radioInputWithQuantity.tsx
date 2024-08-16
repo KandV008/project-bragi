e7e017640a2fbce890f86670da241aa63742323a
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2024-08-16 17:11:07 +0200

    refactor: Update Radio Button Input List Component

---
+++ app/ui/components/inputs/radioInputWithQuantity.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
+
+interface RadioInputListProps {
+  name: string;
+  list: any[];
+  valueOf: (type: string) => string;
+  type: string;
+  onChange?: (type: string) => (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function RadioInputWithQuantity({
+  name,
+  list,
+  valueOf,
+  type,
+  onChange,
+}: RadioInputListProps) {
+  const [selectedValue, setSelectedValue] = useState<string | null>(null);
+  const [currentEvent, setCurrentEvent] = useState<ChangeEvent<HTMLInputElement> | null>(null)
+
+  const handleRadioChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    setCurrentEvent(event)
+    setSelectedValue(newValue);
+    onChange!(type)(event);
+  };
+
+  const unselect = () => {
+    onChange!(type)(currentEvent!)
+    setSelectedValue(null);
+  };
+
+  const buttonState = selectedValue ? "block" : "hidden"
+
+  return (
+    <form action="" className="flex flex-col px-3">
+      {list.map((element, index) =>
+        element.quantity !== 0 ? (
+          <div key={element.type}>
+            <input
+              type="radio"
+              id={name + "-" + index}
+              name={name}
+              value={valueOf(element.type)}
+              checked={selectedValue === valueOf(element.type)}
+              onChange={handleRadioChange}
+            />
+            <label htmlFor={name + "-" + index}>
+              {" "}{element.type} ( {element.quantity} )
+            </label>
+            <br />
+          </div>
+        ) : null
+      )}
+      <div className="self-center">
+        <button type="button" onClick={unselect} className={`${buttonState} hover:underline`}>
+          Quitar opción
+        </button>
+      </div>
+    </form>
+  );
+}
