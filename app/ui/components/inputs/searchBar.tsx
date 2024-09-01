120c1f18872a026e041691b0d9865309318f2c89
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2024-09-01 19:43:16 +0200

    fix: Top Loading Bar not working with Search Bar

--- app/ui/components/inputs/searchBar.tsx
+++ app/ui/components/inputs/searchBar.tsx
@@ -1,40 +1,49 @@
 'use client';
 
+import { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { useRouter } from "next/navigation";
 import { componentText, fillDefaultComponentBackground, hoverFillDefaultComponentBackground, componentBorder } from "../../tailwindClasses";
+import Link from "next/link";
 
-interface SearchBarProps{
-  isCompress: boolean
+interface SearchBarProps {
+  isCompress: boolean;
 }
 
 export default function SearchBar({ isCompress }: SearchBarProps) {
-  const router = useRouter();
+  const [keyword, setKeyword] = useState(''); 
 
-  const placeholder = isCompress ? "Buscar" : "Buscar productos"
+  const placeholder = isCompress ? "Buscar" : "Buscar productos";
+  const id = isCompress ? "compressSearchBar" : "searchBar";
 
-  const activeSearch = (formData: FormData) => {
-    const keyword = formData.get("keyword") as string;
-    router.push(`/search/${keyword}`)
-  }
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); 
+    document.getElementById(id)?.click();
+  };
 
   return (
-      <form action={activeSearch} className={`relative ${componentText}`}>
-        <div className="absolute inset-y-5 flex pointer-events-none left-5">
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-        </div>
-        <input
-          type="text"
-          id="keyword"
-          name="keyword"
-          className={`w-full py-3 text-lg rounded-full pl-14 
-                ${fillDefaultComponentBackground} ${hoverFillDefaultComponentBackground}
-                ${componentBorder}
-                dark:placeholder:text-emerald-100 placeholder:text-emerald-900 placeholder:font-bold`}
-          placeholder={placeholder}
-          required
-        />
-      </form>
+    <form onSubmit={handleSubmit} className={`relative ${componentText}`}>
+      <div className="absolute inset-y-5 flex pointer-events-none left-5">
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </div>
+      <input
+        type="text"
+        id="keyword"
+        name="keyword"
+        value={keyword}
+        onChange={handleInputChange}
+        className={`w-full py-3 text-lg rounded-full pl-14 
+              ${fillDefaultComponentBackground} ${hoverFillDefaultComponentBackground}
+              ${componentBorder}
+              dark:placeholder:text-emerald-100 placeholder:text-emerald-900 placeholder:font-bold`}
+        placeholder={placeholder}
+        required
+      />
+      <Link href={`/search/${encodeURIComponent(keyword)}`} id={id} />
+    </form>
   );
 }
