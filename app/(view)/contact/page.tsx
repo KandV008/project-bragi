11010a81584d9e11b5841e6e4ad5d77b5b7c69df
Krokodile <117300490+KandV008@users.noreply.github.com>
2024-09-19 12:33:43 +0200

    feat: Create Contact Page
    Logic not implemented yet

---
+++ app/(view)/contact/page.tsx
@@ -0,0 +1,65 @@
+import SubmitButton from "@/app/ui/components/buttons/submitButton";
+import TextAreaInput from "@/app/ui/components/inputs/textAreaInput";
+import TextInput from "@/app/ui/components/inputs/textInput";
+import SectionHeader from "@/app/ui/components/tags/sectionHeader";
+import { componentBackground, componentBorder, componentText } from "@/app/ui/tailwindClasses";
+import {
+  faComment,
+  faEnvelope,
+  faPenNib,
+  faUpload,
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function Page() {
+  return (
+    <>
+      <SectionHeader text={"Contacta con nosotros"} />
+      {/* Contact */}
+      <section className="flex flex-col lg:flex-row w-full gap-3">
+        {/* Display */}
+        <article className={`flex flex-col lg:w-1/2 gap-4 lg:gap-8 m-2 md:mt-0 lg:mx-5 px-2 lg:px-5 justify-around
+                            ${componentText} text-lg sm:text-xl lg:text-2xl font-medium  align-middle text-justify `}>
+            <div>En esta página puedes <span className="font-bold">contactar con nosotros</span> para resolver cualquier necesidad que tengas relacioanda con nosotros.</div>
+            <ul className="px-5 lg:px-10 font-semibold">
+                <li>* ¿Quieres contactar con nostros por la razón que sea? </li>
+                <li>* ¿Necesitas resolver alguna duda relacionada con el funcionamiento de la web? </li>
+                <li>* ¿Tienes alguna sugerencia para mejorar la aplicación? </li>
+            </ul>
+            <div> Sea lo que sea, no dudes en escribirnos. Y en <span className="font-bold">menos de 24 horas</span> serás contestado con lo que requieras.</div>
+        </article>
+        {/* Form */}
+        {/* TODO Form Action */}
+        <form
+          className={`flex flex-col gap-5 p-5 sm:p-10 items-center justify-center lg:w-1/2
+                   ${componentBackground}
+                   ${componentBorder} rounded-xl`}
+        >
+          <SectionHeader text={"¿En qué te podemos ayudar?"} />
+          <TextInput
+            name={"email"}
+            type={"text"}
+            placeholder={"ejemplo@email.com"}
+            label={"Correo Electrónico"}
+            icon={faEnvelope}
+          />
+          <TextInput
+            name={"subject"}
+            type={"text"}
+            placeholder={"Razón de contacto"}
+            label={"Asunto del mensaje"}
+            icon={faPenNib}
+          />
+          <TextAreaInput
+            name={"message"}
+            placeholder={"Texto con el mensaje"}
+            label={"Cuerpo del mensaje"}
+            icon={faComment}
+          />
+          <div className="place-self-center">
+            <SubmitButton text={"Enviar"} icon={faUpload} isDisable={false} />
+          </div>
+        </form>
+      </section>
+    </>
+  );
+}
