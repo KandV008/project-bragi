638d0b6b90872e0fd7c81ae8d40a5e334fe65a08
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-03-06 22:15:59 +0100

    feat: Describe Filter Tests

---
+++ app/ui/containers/search/filter/filter.test.tsx
@@ -0,0 +1,19 @@
+describe("<Filter />", () => {
+    it.skip("should render Filter with the attributes from a EARPHONE Product", () => {
+      // TODO End test
+    });
+  
+    it.skip("should render Filter with the attributes from a ACCESSORY Product", () => {
+        // TODO End test
+    });
+
+    it.skip("should press one filter and check if appears an option that removes the selection", () => {
+        // TODO End test
+    });
+
+    it.skip("should press unselect and check if doesn't appears an option that removes the selection", () => {
+        // TODO End test
+    });
+
+  });
+  
