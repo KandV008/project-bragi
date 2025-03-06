b91352d29e91e8c33ccafe918b8cdfb31f6e4574
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-03-06 12:00:56 +0100

    feat: Describe Product Shopping List tests

--- app/ui/components/products/productShoppingList/productShoppingList.test.tsx
+++ app/ui/components/products/productShoppingList/productShoppingList.test.tsx
@@ -1,5 +1,17 @@
 describe("<ProductShoppingList />", () => {
-    it.skip("", () => { // TODO End test
+  it.skip("should render ProductShoppingList with name, brand, price, earside, guarantee, color and quantity", () => {
+    // TODO End test
+  });
 
-    })
-})
+  it.skip("should increse in one the quantity of the product in the shopping list", () => {
+    // TODO End test
+  });
+
+  it.skip("should decrese in one the quantity of the product in the shopping list", () => {
+    // TODO End test
+  });
+
+  it.skip("should remove the product product from the shopping list when the quantity is zero", () => {
+    // TODO End test
+  });
+});
