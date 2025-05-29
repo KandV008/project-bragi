54c81195eb609d8fc38770337b454cb613bfec43
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-05-29 11:59:51 +0200

    test: Create User Navigation to Novelty test

--- tests/e2e/navigation.spec.ts
+++ tests/e2e/navigation.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { SYSTEM_TEST_TAG } from '../testConstants';
 
-test(`[${SYSTEM_TEST_TAG}] Unregistered User navigation`, async ({ page }) => {
+test(`[${SYSTEM_TEST_TAG}] Unregistered User navigation to Product`, async ({ page }) => {
     await page.goto('https://audifonosxmenos.com');
 
     await page.getByRole('button', { name: 'Audífonos' }).first().click();
@@ -11,4 +11,17 @@ test(`[${SYSTEM_TEST_TAG}] Unregistered User navigation`, async ({ page }) => {
     await page.waitForURL('**/search/about/**');
 
     await expect(page.getByRole('heading', { name: 'Descripción' })).toBeVisible();
+})
+
+test(`[${SYSTEM_TEST_TAG}] Unregistered User navigation to Novelty`, async ({ page }) => {
+    await page.goto('https://audifonosxmenos.com');
+
+    await page.getByRole('button', { name: 'Servicios' }).first().click();
+    await page.waitForURL('**/services');
+
+    await page.getByText('Novedades').first().click();
+    await page.waitForURL('**/services/novelties');
+
+    await expect(page.getByRole('heading').first()).toBeVisible();
+})
 })
