a36a13f5a2910af7c55421bb445e426b1faf2c41
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-12-30 10:05:54 +0100

    feat: Disable adding product to special cases

--- app/ui/components/buttons/submitButton/submitButton.tsx
+++ app/ui/components/buttons/submitButton/submitButton.tsx
@@ -1,4 +1,10 @@
-import { negativeComponentText, negativeComponentBackground, negativeHoverComponentBackground } from "@/app/ui/tailwindClasses";
+import {
+  negativeComponentText,
+  negativeComponentBackground,
+  negativeHoverComponentBackground,
+  disableComponentBackground,
+  disableComponentBorder,
+} from "@/app/ui/tailwindClasses";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -24,10 +30,10 @@ interface SubmitButtonProps {
 
 /**
  * A button component that displays an icon and text, and can be disabled based on the `isDisable` prop.
- * 
+ *
  * If `isDisable` is true, the button is disabled and styled as inactive.
  * If `isDisable` is false, the button is enabled and clickable.
- * 
+ *
  * @param text - The text to display on the button.
  * @param icon - The icon to display inside the button.
  * @param isDisable - A boolean to control whether the button is disabled.
@@ -38,43 +44,31 @@ export default function SubmitButton({
   icon,
   isDisable,
 }: SubmitButtonProps) {
+  const style = isDisable
+    ? `${disableComponentBackground} ${disableComponentBorder}`
+    : `${negativeComponentBackground} ${negativeHoverComponentBackground}`;
+
   return (
-    <>
-      {isDisable ? (
-        <button
-          disabled
-          className={`w-64 sm:w-80 h-12 flex flex-row place-self-center md:place-self-start justify-center rounded 
-                  bg-gray-500 dark:bg-gray-300 ${negativeComponentText}`}
-        >
-          <div className="flex flex-row place-self-center gap-3">
-            <div className=" mr-0 md:mr-2 xl:mr-0">
-              <FontAwesomeIcon icon={icon} className="" />
-            </div>
-            <span className="text-base font-black">{text}</span>
-          </div>
-        </button>
-      ) : (
-        <button
-          type="submit"
-          className={`w-64 sm:w-72 h-12 flex flex-row place-self-center md:place-self-start justify-center rounded 
-                  ${negativeComponentBackground} ${negativeHoverComponentBackground}
+    <button
+      type="submit"
+      className={`w-64 sm:w-72 h-12 flex flex-row place-self-center md:place-self-start justify-center rounded 
+                  ${style}
                   ${negativeComponentText}`}
-        >
-          <div className="flex flex-row place-self-center gap-3">
-            <div className=" mr-0 md:mr-2 xl:mr-0">
-              <FontAwesomeIcon icon={icon} className="" />
-            </div>
-            <span className="text-base font-black">{text}</span>
-          </div>
-        </button>
-      )}
-    </>
+      disabled={isDisable}
+    >
+      <div className="flex flex-row place-self-center gap-3">
+        <div className=" mr-0 md:mr-2 xl:mr-0">
+          <FontAwesomeIcon icon={icon} className="" />
+        </div>
+        <span className="text-base font-black">{text}</span>
+      </div>
+    </button>
   );
 }
 
 /**
  * Skeleton component for the SubmitButton, used to display a placeholder during loading.
- * 
+ *
  * @returns A skeleton representation of the SubmitButton with a gray background and basic button shape.
  */
 export function SubmitButtonSkeleton() {
